ced1b9251e53b8a3651ceac6d9ce0bcaccc2617d
AnujShakya123 <130181154+AnujShakya123@users.noreply.github.com>
2023-05-10 01:13:03 +0530

    comment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly person = new RWOnly();
+        person.setName("Anuj");
+        person.setAge(25);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
 }
